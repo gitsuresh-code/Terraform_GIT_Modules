107e04e414d1950a4a8662fe12745580499ba8f7
SureshU <sureshdevops@gmail.com>
2025-10-25 21:27:35 +0530

    message

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -175,11 +175,11 @@ resource "aws_route" "database" {
 }
 
 
-# resource "aws_route_table_association" "public" {
-#   count = length(var.public_subnet_cidrs)
-#   subnet_id      = aws_subnet.public[count.index].id
-#   route_table_id = aws_route_table.public.id
-# }
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
 
 # resource "aws_route_table_association" "private" {
 #   count = length(var.private_subnet_cidrs)
