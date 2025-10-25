07bd95d8a7e20e03abac15de5480c83c31d0d3dc
SureshU <sureshdevops@gmail.com>
2025-10-25 14:41:53 +0530

    message

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,16 +1,16 @@
 # VPC
-resource "aws_vpc" "roboshop_vpc" {
+resource "aws_vpc" "suresh_vpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
   tags = merge(
-    var.vpc_tags,  #called from variables
+    var.vpc_tags,  #called from module variables
     local.common_tags, #called from locals
     {
         Name = local.common_name_suffix
     }
   )
-  
+
 }
 
